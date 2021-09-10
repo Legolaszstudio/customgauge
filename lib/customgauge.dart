2be21a92cf94d9a137e87eec4830eac4e2ffa69c
Levente Novotny <Novotny.Levente@gmail.com>
2021-09-10 21:03:11 +0200

    Add nullsafety

--- lib/customgauge.dart
+++ lib/customgauge.dart
@@ -111,7 +111,7 @@ class CustomGauge extends StatefulWidget {
   ///painted in defaultSegmentColor
   ///
   ///Each segment is represented by a GaugeSegment object that has a name, segment size and color
-  final List<GaugeSegment> segments;
+  final List<GaugeSegment>? segments;
 
   ///Supply a min value for the Gauge. Defaults to 0
   final double minValue;
@@ -120,7 +120,7 @@ class CustomGauge extends StatefulWidget {
   final double maxValue;
 
   ///Current value of the Gauge
-  final double currentValue;
+  final double? currentValue;
 
   ///Custom color for the needle on the Gauge. Defaults to Colors.black
   final Color needleColor;
@@ -130,10 +130,10 @@ class CustomGauge extends StatefulWidget {
 
   ///Widget that is used to show the current value on the Gauge. Defaults to show the current value as a Decimal with 1 digit
   ///If value must not be shown, supply Container()
-  final Widget valueWidget;
+  final Widget? valueWidget;
 
   ///Widget to show any other text for the Gauge. Defaults to Container()
-  final Widget displayWidget;
+  final Widget? displayWidget;
 
   ///Specify if you want to display Min and Max value on the Gauge widget
   final bool showMarkers;
@@ -197,13 +197,13 @@ class _CustomGaugeState extends State<CustomGauge> {
 
   @override
   Widget build(BuildContext context) {
-    List<GaugeSegment> _segments = widget.segments;
-    double _currentValue = widget.currentValue;
+    List<GaugeSegment>? _segments = widget.segments;
+    double? _currentValue = widget.currentValue;
 
-    if (widget.currentValue < widget.minValue) {
+    if (widget.currentValue! < widget.minValue) {
       _currentValue = widget.minValue;
     }
-    if (widget.currentValue > widget.maxValue) {
+    if (widget.currentValue! > widget.maxValue) {
       _currentValue = widget.maxValue;
     }
 
@@ -252,7 +252,7 @@ class _CustomGaugeState extends State<CustomGauge> {
             alignment: Alignment.center,
             child: Transform.rotate(
               angle: (math.pi / 4) +
-                  ((_currentValue - widget.minValue) /
+                  ((_currentValue! - widget.minValue) /
                       (widget.maxValue - widget.minValue) *
                       1.5 *
                       math.pi),
